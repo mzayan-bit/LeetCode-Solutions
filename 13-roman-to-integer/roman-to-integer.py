1d9f5da9239c3414901aac10c01b6f1cc559748e
Muhammad Zayan <muhammadzayan2004@gmail.com>
2026-02-27 23:41:42 +0500

    Time: 7 ms (42.28%) | Memory: 19.2 MB (85.50%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        record = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        total = 0
+        for i in range(len(s)):
+            if i< len(s) - 1 and record[s[i]] < record[s[i + 1]]:
+                total -= record[s[i]]
+            else:
+                total += record[s[i]]
+        return total
