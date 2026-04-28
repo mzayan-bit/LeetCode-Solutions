bdae676375064195358f1d741906c40a46525fab
Muhammad Zayan <muhammadzayan2004@gmail.com>
2026-04-29 01:40:47 +0500

    Time: 0 ms (100.00%) | Memory: 19.4 MB (17.02%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = {}
+        def solve(n):
+            if n <= 2:
+                return n
+            if n in memo:
+                return memo[n]
+            memo[n] = solve(n-1) + solve(n-2)
+            return memo[n]
+        return solve(n)
